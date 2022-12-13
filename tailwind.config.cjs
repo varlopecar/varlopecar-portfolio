467339e84c24272a50f60f7d103540be8dcd9bb0
Carlos Vargas <varlopecar@gmail.com>
2022-12-13 16:11:56 +0100

    chore: first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			screens: {
+				'sm' : '640px',
+				'md' : '768px',
+				'lg' : '1024px',
+				'xl' : '1280px',
+				'2xl' : '1536px'
+			},
+			colors: {
+				'primary': '#E5BA73',
+				'primary-light': '#FAEAB1',
+				'primary-dark': '#C98919',
+				'accent': '#F2F2F2',
+				'accent-light': '#FFFFFF',
+				'accent-dark': '#C9C9C9',
+				'dark': '#202023',
+				'dark-light': '#3C3C3F',
+				'blue-accent': '#00E7FF',
+				'rose-accent': '#FF00E7'
+			},
+			fontFamily: {
+				'body': ['Lato', 'sans-serif'],
+				'heading': ['Montserrat', 'sans-serif'],
+				'code': ['Roboto Mono', 'monospace']
+			}
+		},
+	},
+	plugins: [],
+}
